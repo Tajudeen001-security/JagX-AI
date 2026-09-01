69d105d3f7be8df298939a9f06af98686cf494cf
Tajudeen001-security <gbadamositajudeenwan@gmail.com>
2026-09-01 18:48:38 +0100

    Add centralized runtime security policy

---
+++ security/policy.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from enum import Enum
+
+class Capability(str, Enum):
+    READ_FILES='read_files'
+    WRITE_FILES='write_files'
+    RUN_LOCAL='run_local'
+    NETWORK='network'
+    FINANCIAL='financial'
+    SECURITY_TEST='security_test'
+    DEPLOY='deploy'
+
+@dataclass(frozen=True)
+class SecurityPolicy:
+    allowed: frozenset[Capability] = frozenset({Capability.READ_FILES, Capability.WRITE_FILES, Capability.RUN_LOCAL})
+    require_confirmation: frozenset[Capability] = frozenset({Capability.NETWORK, Capability.FINANCIAL, Capability.SECURITY_TEST, Capability.DEPLOY})
+
+    def check(self, capability: Capability, confirmed: bool = False) -> bool:
+        if capability not in self.allowed:
+            return False
+        if capability in self.require_confirmation and not confirmed:
+            return False
+        return True
+
+    def with_capability(self, capability: Capability) -> 'SecurityPolicy':
+        return SecurityPolicy(self.allowed | {capability}, self.require_confirmation)
